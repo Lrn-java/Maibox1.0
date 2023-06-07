4b70cfaddc20aed865472b711c54bc84dfbe53f5
lrn--hy <6058acabd6e744529f1c8b120807acd2@example.com>
2023-06-07 16:40:37 +0800

    更新 RegistrationInformation.java

--- src/Maibox/li/GetData/RegistrationInformation.java
+++ src/Maibox/li/GetData/RegistrationInformation.java
@@ -16,81 +16,12 @@ import static java.lang.System.out;
 public interface RegistrationInformation {
 
     /**
-     * @param user 该参数用来获取用户信息
-     *             获取信息
-     */
-    default void getMassge(String user) {
-
-        String listing = new String("C:\\Users\\Lrn\\Desktop");
-        File file = new File(listing,"File数据库.txt");
-        FileWriter fileWriter = null;
-        BufferedWriter bufferedWriter = null;
-        try {
-
-            //判断是否有此路径。创建路径
-            if(!file.exists()){
-                file.mkdirs();
-            }
-
-
-            fileWriter = new FileWriter(file, true);
-            bufferedWriter = new BufferedWriter(fileWriter);
-            //使用集合类来保存值
-
-            FileReader fileReader = new FileReader("C:\\Users\\Lrn\\Desktop\\File数据库.txt");
-
-            /*预留问题
-            BufferedReader bufferedReader = new BufferedReader(fileReader);
-            String line;
-            */
-
-            bufferedWriter.write(user + "\n");
-            //刷新
-            bufferedWriter.flush();
-
-            //关闭流
-            bufferedWriter.close();
-            fileWriter.close();
-
-            try {
-
-                InetAddress localhost = InetAddress.getLocalHost();
-
-                String userName = "root";
-                String password = "123456";
-                String databasesURL = "jdbc:mysql://"+localhost.getHostAddress()+":3306/user_message";
-                String IP = "jdbc:mysql://"+localhost.getHostAddress()+":3306/user_message";
-                out.println(IP);
-
-                //加载驱动
-                Class.forName("com.mysql.cj.jdbc.Driver");
-
-                //创建连接
-                Connection connection = DriverManager.getConnection(userName,password,databasesURL);
-
-                //获取的用户名和密码存入数据库中
-                String insertQuery = "insert into usermessage (user,password) values (user,name)";  
-                
-            } catch (ClassNotFoundException | SQLException e) {
-                e.printStackTrace();
-            } catch (UnknownHostException e) {
-                throw new RuntimeException(e);
-            }
-
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-    /**
       *这个方法会连接到数据库，这个操作会将获取到的参数写入数据库中
       *有两个参数，被调用后会将信息写入数据库
       *usermessage：用户名
       *password：密码
       */
-    default void connection(String usermessage,String password){
-        if(!user.equalse("") && user != null){
-            //预留问题
-        }else{
+    default void getMassge(String usermessage,String password){
             //写入到user_message数据库中，这个数据库中有一个表，用来保存用户名和密码这些基本信息
         try {
 
@@ -103,7 +34,6 @@ public interface RegistrationInformation {
                 String userName = "root";
                 String password = "758206lrnandlnxA";
                 String databasesURL = "jdbc:mysql://"+localhost.getHostAddress()+":3306/user_message";
-                String IP = "jdbc:mysql://"+localhost.getHostAddress()+":3306/user_message";
 
                 //创建连接
                 Connection connection = DriverManager.getConnection(userName,password,databasesURL);
@@ -116,9 +46,7 @@ public interface RegistrationInformation {
             }
 
         } catch (IOException e) {
-            //抛出异常
             throw new RuntimeException(e);
         }
     }
-}
 }
